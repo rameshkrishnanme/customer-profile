3ace125e511099988c9c62738d038b392d179919
Ramesh Krishnan <M1013624@mindtree.com>
2017-06-21 21:04:04 +0100

    war changes

--- src/main/java/com/app/micro/customerprofile/SprintBaseRootApplication.java
+++ src/main/java/com/app/micro/customerprofile/SprintBaseRootApplication.java
@@ -1,11 +1,18 @@
 package com.app.micro.customerprofile;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SprintBaseRootApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SprintBaseRootApplication.class, args);
-	}
+public class SprintBaseRootApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBaseRootApplication.class, args);
+    }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SprintBaseRootApplication.class);
+    }
+
 }
